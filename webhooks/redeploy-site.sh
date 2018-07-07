a2680855c18f4b5a480b44916484baf935d56851
Abhinav Sarkar <abhinav@abhinavsarkar.net>
2018-07-07 10:01:38 +0000

    Adds email notification for site deploy

--- webhooks/redeploy-site.sh
+++ webhooks/redeploy-site.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 docker-compose pull site
-docker-compose up -d --build site 
+docker-compose up -d --build site
+echo "Site deployed" | mailx -v -r "hooks@abhinavsarkar.net" -s "Site deployed" -S smtp="smtp:25" "abhinav.sarkar@gmail.com"
