81aa4e012f5662175183a407a7ec0ee6b69f342b
Abhinav Sarkar <abhinav@abhinavsarkar.net>
2018-10-28 12:46:06 +0000

    Make site redeploy recreate photos volume

--- webhooks/redeploy-site.sh
+++ webhooks/redeploy-site.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
 docker-compose pull site
+docker-compose stop site
+docker-compose rm -f site
+docker volume rm -f space_photothumbs
 docker-compose up -d --build site
 echo "Site deployed" | mailx -v -r "hooks@abhinavsarkar.net" -s "Site deployed" -S smtp="smtp:25" "abhinav.sarkar@gmail.com"
